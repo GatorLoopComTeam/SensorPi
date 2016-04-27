72ea9864b28ed0f7614b97266b5b3d4fc0a9edfa
hyperloop <hyperloop>
2016-04-27 04:24:40 +0000

    client

--- client_socket_test.py
+++ client_socket_test.py
@@ -3,14 +3,14 @@ import random
 s = socket.socket()
 address = "10.2.1.62"
 port = 9000
+s.connect((address,port))
 for x in range(0,100):
     try:
         for i in range(0,100):
-            s.connect((address,port))
             s.send(("a;" + str(round(random.random(), 5)) + "," + str(round(random.random(), 5))).encode('ascii'))
-            s.close()
     except Exception as e: 
         print("something's wrong with %s:%d. Exception is %s" % (address, port, e))
-    finally:
-        s.close()
+#    finally:
+#        s.close()
     time.sleep(5)
+s.close()
